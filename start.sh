bc85f04c43e4c8d0b47ef9907613e226d8c23c47
zoxibox <djzoxi@gmail.com>
2015-04-10 11:51:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
+chown -R nobody:users /etc/minidlna.conf
 
 # set media to point at /media docker volume
-sed -i 's/media_dir=\/opt/media_dir=\/media/g' /etc/minidlna.conf
+sed -i 's/media_dir=\/var\/lib\/minidlna/media_dir=\/media/g' /etc/minidlna.conf
 
 # set logs to point at /config docker volume
 sed -i 's/#log_dir=\/var\/log/log_dir=\/config/g' /etc/minidlna.conf
 
 # set db to point at /config docker volume
-sed -i 's/#db_dir=\/var\/cache\/minidlna/db_dir=\/config/g' /etc/minidlna.conf
+sed -i 's/#db_dir=\/var\/lib\/minidlna/db_dir=\/config/g' /etc/minidlna.conf
 
 # set process to run as user nobody
-sed -i 's/user=minidlna/user=nobody/g' /etc/minidlna.conf
+#sed -i 's/user=minidlna/user=nobody/g' /etc/minidlna.conf
 
 # set friendly name to MiniDLNA
-sed -i 's/#friendly_name=My DLNA Server/friendly_name=MiniDLNA/g' /etc/minidlna.conf
+sed -i 's/#friendly_name=/friendly_name=MiniDLNA/g' /etc/minidlna.conf
 
 # if minidlna config file doesnt exist then copy default to host config volume
 if [ ! -f "/config/minidlna.conf" ]; then
