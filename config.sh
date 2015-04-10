dd895e9acdca0a822a62f6ec536f6d5cea80974d
zoxibox <djzoxi@gmail.com>
2015-04-10 12:19:02 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # set media to point at /media docker volume
 sed -i 's/media_dir=\/var\/lib\/minidlna/media_dir=\/media/g' /etc/minidlna.conf
 
@@ -7,8 +9,8 @@ sed -i 's/#log_dir=\/var\/log/log_dir=\/config/g' /etc/minidlna.conf
 # set db to point at /config docker volume
 sed -i 's/#db_dir=\/var\/lib\/minidlna/db_dir=\/config/g' /etc/minidlna.conf
 
-# set process to run as user nobody
-#sed -i 's/user=minidlna/user=nobody/g' /etc/minidlna.conf
+# set automatic discovery of new files in the media_dir directory
+sed -i 's/#inotify=yes/inotify=yes/g' /etc/minidlna.conf
 
 # set friendly name to MiniDLNA
 sed -i 's/#friendly_name=/friendly_name=MiniDLNA/g' /etc/minidlna.conf
