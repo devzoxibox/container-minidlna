158282a2e0f6395377917b371249319105af5331
zoxibox <djzoxi@gmail.com>
2015-04-10 12:01:41 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,14 @@
+# set media to point at /media docker volume
+sed -i 's/media_dir=\/var\/lib\/minidlna/media_dir=\/media/g' /etc/minidlna.conf
+
+# set logs to point at /config docker volume
+sed -i 's/#log_dir=\/var\/log/log_dir=\/config/g' /etc/minidlna.conf
+
+# set db to point at /config docker volume
+sed -i 's/#db_dir=\/var\/lib\/minidlna/db_dir=\/config/g' /etc/minidlna.conf
+
+# set process to run as user nobody
+#sed -i 's/user=minidlna/user=nobody/g' /etc/minidlna.conf
+
+# set friendly name to MiniDLNA
+sed -i 's/#friendly_name=/friendly_name=MiniDLNA/g' /etc/minidlna.conf
